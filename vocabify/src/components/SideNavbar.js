5956fd9512f9c3f247acaccd774f3c5418a89fb3
Ethan Handojo <ehandojo21@gmail.com>
2024-03-04 21:24:53 -0600

    Upload page first side nav bar

---
+++ vocabify/src/components/SideNavbar.js
@@ -0,0 +1,107 @@
+import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { Disclosure } from "@headlessui/react";
+import {
+  MdOutlineSpaceDashboard,
+  MdOutlineAnalytics,
+  MdOutlineIntegrationInstructions,
+  MdOutlineMoreHoriz,
+  MdOutlineSettings,
+  MdOutlineLogout,
+  MdOutlineUpload,
+  MdOutlineNote,
+  MdOutlineLightbulb,
+} from "react-icons/md";
+import { CgProfile } from "react-icons/cg";
+import { FaRegComments } from "react-icons/fa";
+import { BiMessageSquareDots } from "react-icons/bi";
+
+
+function SideNavbar() {
+  return (
+    <div>
+      <Disclosure as="nav">
+        <Disclosure.Button className="absolute top-4 right-4 inline-flex items-center peer justify-center rounded-md p-2 text-gray-800 hover:bg-gray-900 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white group">
+          <GiHamburgerMenu
+            className="block md:hidden h-6 w-6"
+            aria-hidden="true"
+          />
+        </Disclosure.Button>
+        <div className="p-6 w-1/2 h-screen bg-white z-20 fixed top-0 -left-96 lg:left-0 lg:w-60  peer-focus:left-0 peer:transition ease-out delay-150 duration-200">
+          <div className="flex flex-col justify-start item-center">
+            <h1 className="text-base text-center cursor-pointer font-bold text-blue-900 border-b border-gray-100 pb-4 w-full">
+              Virtual Dashboard
+            </h1>
+            <div className=" my-6 border-b border-gray-100 pb-4">
+              <div className="flex mb-6 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineSpaceDashboard className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  Dashboard
+                </h3>
+              </div>
+              <div className="flex  mb-6 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <CgProfile className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  Profile
+                </h3>
+              </div>
+              <div className="flex  mb-6 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <FaRegComments className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  Support
+                </h3>
+              </div>
+            </div>
+            {/*Resourses */}
+            <div className=" my-4 border-b border-gray-100 pb-4">
+              <div className="flex mb-6 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineSettings className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  Classes
+                </h3>
+              </div>
+              <div className="flex mb-6 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineNote className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  Flashcards
+                </h3>
+              </div>
+              <div className="flex mb-6 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineUpload className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  Upload
+                </h3>
+              </div>
+            </div>
+            {/* setting  */}
+            <div className=" my-4 border-b border-gray-100 pb-4">
+              <div className="flex mb-6 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineSettings className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  Settings
+                </h3>
+              </div>
+              <div className="flex mb-6 justify-start items-center gap-4 pl-5 hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineMoreHoriz className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  More
+                </h3>
+              </div>
+            </div>
+            {/* logout */}
+            <div className=" my-4">
+              <div className="flex mb-6 justify-start items-center gap-4 pl-5 border border-gray-200  hover:bg-gray-900 p-2 rounded-md group cursor-pointer hover:shadow-lg m-auto">
+                <MdOutlineLogout className="text-2xl text-gray-600 group-hover:text-white " />
+                <h3 className="text-base text-gray-800 group-hover:text-white font-semibold ">
+                  Logout
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Disclosure>
+    </div>
+  );
+}
+
+export default SideNavbar;
